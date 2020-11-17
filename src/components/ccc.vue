45a2521722f4dae65b8f617193fcf48bce980fe2
luoc <418866516@qq.com>
2020-11-17 19:06:41 +0800

    1

---
+++ src/components/ccc.vue
@@ -0,0 +1,27 @@
+<template>
+    <div @click="add">8888888888888{{num}}{{msg}}</div>
+</template>
+<script>
+export default {
+    props:{
+        msg:String
+    },
+    data(){
+        return{
+            num:1,
+        }
+    },
+    mounted(){
+
+    },
+    methods:{
+        add(){
+            console.log('我点击啦')
+            this.num++
+        },
+    },
+    updated(){
+        console.log('我======跟新啦')
+    },
+}
+</script>
